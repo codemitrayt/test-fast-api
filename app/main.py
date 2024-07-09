a9c3779fbd23b4a2cf89ecd8081d3cd68238b82a
codemitrayt <codemitrayt@gmail.com>
2024-07-09 19:24:50 +0530

    feat: add CRUD routes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI # type: ignore 
+from fastapi import FastAPI
+from .models.todos import Todo
+from .configs.db import collection_name
+from .schema.schema import list_serial
+from bson import ObjectId
 
-app = FastAPI() 
-
+app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'message': 'Hello World'}
+    return {'message': 'Hello, World!'}
+
+@app.get('/todos')
+async def get_todos():
+    todos = list_serial(collection_name.find())
+    return todos
+
+@app.post('/todos')
+async def create_todo(todo: Todo):
+      collection_name.insert_one(dict(todo))
+
+@app.put('/todos/${id}')
+async def update_todo(id: str, todo : Todo):
+    collection_name.find_one_and_update({"_id" : ObjectId(id)}, {"$set" : dict(todo)})
+  
+
+@app.delete('/todos/{id}')
+async def delete_todo(id: str):
+    collection_name.find_one_and_delete({"_id" : ObjectId(id)})
